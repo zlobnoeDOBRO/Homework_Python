cf68c6f28e5bc57ab0ee145db8e7455923bb33d9
Anton <streetneo21@gmail.com>
2025-09-11 04:10:47 +0300

    Урок 5 выполнен

---
+++ lesson_06/task3_wait_images.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Chrome()
+
+try:
+    driver.get(
+        "https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
+
+    # Ждем пока исчезнет спиннер загрузки
+    wait = WebDriverWait(driver, 10)
+    wait.until(
+        EC.invisibility_of_element_located((By.CSS_SELECTOR, "#spinner"))
+    )
+
+    # Получаем третью картинку
+    third_image = driver.find_element(
+        By.CSS_SELECTOR, "#image-container img:nth-child(3)")
+    print("Атрибут src третьей картинки:", third_image.get_attribute("src"))
+
+except Exception as e:
+    print(f"Произошла ошибка: {e}")
+
+    driver.quit()
